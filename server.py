af879afe7625dc273a7be748d66604c962dacc8d
Blessed Mutengwa <blessedmutengwa@gmail.com>
2021-06-20 17:13:57 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -2,13 +2,11 @@ import streamlit as st
 import cv2 as cv
 import tempfile
 import warnings
-warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
+import os
+from PIL import Image
 
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import decode_predictions
-import numpy as np
+
+warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
 
 
 # hide hamburger menu
@@ -29,61 +27,27 @@ hide_footer_style = """
 st.markdown(hide_footer_style, unsafe_allow_html=True)
 
 
-detected_obj = ['sports_car', 'lane']
-
-
-def upload_video():
-    pass
-
-def home():
-    pass
-
-model = VGG16(weights='imagenet', include_top=True)
-
-def identify(img_path): 
-
-  img = image.load_img(img_path, color_mode='rgb', target_size=(224,224)) 
-
-  # Resizing to fit into VGGNET
-  x = image.img_to_array(img)
-  x.shape
-  x = np.expand_dims(x, axis=0)
-
-  # Using Pre-Trained model for prediction
-  x = preprocess_input(x)
-  features = model.predict(x)
-  p = decode_predictions(features)
-  return p
-
+files = [f for f in os.listdir("./encoded_images") if os.path.isfile(os.path.join("./encoded_images", f))]
+fi = [i.split()[0] for i in files] 
+detected_obj = list(set(fi))
 
 def play_video(vid_path):
+    video_file = open(vid_path, 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+  
 
-    vf = cv.VideoCapture(vid_path)
-
-    stframe = st.empty()
-
-    font_scale = 3
-    font = cv.FONT_HERSHEY_PLAIN
-
-    while vf.isOpened():
-        x,y = 0,0
-        ret, frame = vf.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-
-        p = decode_predictions(identify(frame))
-        label = identify()[0][0][1]
-        cv.putText(frame, "[*Object Detected] : "+str(label), (x+10, y+40), font, fontScale=font_scale,color=(0,255,0), thickness=3)
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        stframe.image(gray)
+def filter_frame(txt_search):
+    files = [f for f in os.listdir("./encoded_images") if os.path.isfile(os.path.join("./encoded_images", f))]
+    files.sort(key = lambda x: int(x.split()[1][:-4]))
+    indices = [i for i, s in enumerate(files) if txt_search in s]
+    return files[indices[0]]
 
 
 def main():
     global rez
 
-    rez = "Not Found"
+    rez = "..."
 
     st.sidebar.title("Dashboard")
 
@@ -91,22 +55,7 @@ def main():
     upload = st.sidebar.button('Upload video',key='upload')
 
 
-    if home:
-
-        st.title('Object(s) Detection in video')
-
-        c =  st.beta_container()
-
-        col1, col2 = st.beta_columns([2,6])
-
-        with col1:
-            st.write("Objects in video")
-            for i in range(len(detected_obj)):
-                st.write(str(i+1) +  " "+str(detected_obj[i]))
-
-        with col2:
-            play_video("cars.mp4")
-    else:
+    if upload:
         f = st.file_uploader("Upload video")
 
         s1, s2, s3 = st.beta_columns([1,4,2])
@@ -148,25 +97,62 @@ def main():
 
                     stframe = st.empty()
 
-                    font_scale = 3
-                    font = cv.FONT_HERSHEY_PLAIN
 
-                    while vf.isOpened():
-                        x,y = 0,0
-                        ret, frame = vf.read()
-                        # if frame is read correctly ret is True
-                        if not ret:
-                            print("Can't receive frame (stream end?). Exiting ...")
-                            break
+    else:
 
-                        p = decode_predictions(identify(frame))
-                        label = identify()[0][0][1]
+        st.title('Object(s) Detection in video')
 
-                        # if (len(p[0][0][0]) != 0):
-                        cv.putText(frame, "[*Object Detected] : " + str(label), (x+10, y+40), font, fontScale=font_scale,color=(255,255,0), thickness=3)
-                        gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-                        stframe.image(gray)
+        hs1, hs2, hs3 = st.beta_columns([1,4,2])
 
+        with hs2:
+            h_search = st.text_input("Search for objects in video")
+            if h_search in detected_obj:
+                rez = "found"
+            else:
+                rez = "not found"
+        
+        with hs3:
+            st.write('Search results')
+            if h_search is None:
+                status = st.write(f'{h_search} ...')
+            else:
+                status = st.write(f'{h_search} {rez}')
+
+
+        if len(detected_obj) > 6:
+            col1, col2, col3 = st.beta_columns([2,2,6])
+            with col1:
+                st.subheader("Objects in video")
+                for i in range(0,7):
+                    st.write(str(i+1) +  " "+str(detected_obj[i]))
+            with col2:
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
+                for i in range(7,len(detected_obj)-1):
+                    st.write(str(i+1) +  " "+str(detected_obj[i]))
+            with col3:
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
+                if h_search in detected_obj:
+                    rez = "found"
+                    img = Image.open('./encoded_images/'+str(filter_frame(h_search)))
+                    st.image(img, caption=h_search + " frame found")
+                else:
+                    play_video("./videos/video.mp4")
+        else:
+            col1, col2 = st,beta_columns([2,6])
+            with col1:  
+                st.write("Objects in video")
+                for i in range(len(detected_obj)):
+                    st.write(str(i+1) +  " "+str(detected_obj[i]))
+            with col2:
+                play_video("./videos/video.mp4")
+    
+                    
 
     # else:
     #     st.title('Object Detection using VGG16 Model...')
